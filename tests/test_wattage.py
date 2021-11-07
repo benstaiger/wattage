9e9ff533df528d1de023018e6361d251a8241704
bstaiger <bstaiger@data.cs.purdue.edu>
2021-11-07 12:20:50 -0600

    add simple testing for wattage

--- tests/test_wattage.py
+++ tests/test_wattage.py
@@ -1,1 +1,25 @@
+import pytest
+
 from wattage import wattage
+
+
+def test_wattage_simple():
+    air_density = 1.226  # kg/m**3, average @ sea-level
+    rider_mass = 75  # kg
+    bike_mass = 8  # kg
+    crr = 0.004
+    cda = 0.25
+    gradient = 0
+    wind_speed = 0  # m/s
+    speed = 40 * 1000.0 / 60.0 / 60.0  # 40kph in m/s
+    watts = wattage.wattage_requirement(
+        target_velocity=speed,
+        acceleration=0,
+        slope=gradient,
+        total_mass=rider_mass+bike_mass,
+        relative_wind=wind_speed,
+        c_rolling=crr,
+        cda=cda,
+        air_density=air_density
+    )
+    assert pytest.approx(watts, rel=0.01) == 246.395
--- wattage/wattage.py
+++ wattage/wattage.py
@@ -16,7 +16,7 @@ def wattage_requirement(target_velocity,
     air_speed = (target_velocity - relative_wind)
     aero_wattage = 0.5 * cda * air_density * air_speed**2 * target_velocity
     total = rr_wattage + pe_wattage + ke_wattage + aero_wattage
-    print(f"RR:{rr_wattage:0.1f} PE:{pe_wattage:0.1f} KE:{ke_wattage:0.1f} AE:{aero_wattage:0.1f} TOT:{total:0.1f}")
+    # print(f"RR:{rr_wattage:0.1f} PE:{pe_wattage:0.1f} KE:{ke_wattage:0.1f} AE:{aero_wattage:0.1f} TOT:{total:0.1f}")
     return total
 
 
@@ -26,15 +26,3 @@ def wattage_steady_state(target_velocity,
                          cda,
                          air_density):
     return wattage_requirement(target_velocity, 0, 0, total_mass, 0, c_rolling, cda, air_density)
-
-
-if __name__ == "__main__":
-    speed = 40.0 * 1000 / 60.0 / 60.0 # 40 kph in m/s
-    rider_mass = 80  # kg
-    bike_mass = 9  # kg
-    kit_mass = 1  # kg
-    crr = 0.005
-    cda = 0.27  # m**2
-    p = 1.23923  # kg/m**3 @ 1017.1 mb pressure, 54F temp, 37% humid, 28F dew
-    print(wattage_steady_state(speed, rider_mass+bike_mass+kit_mass, crr, cda, p))
- 
