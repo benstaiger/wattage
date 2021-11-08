be28e06fb384292923784f300cdcab5b71f78117
bstaiger <bstaiger@data.cs.purdue.edu>
2021-11-08 09:09:52 -0600

    compute velocity at a given wattage

--- tests/test_wattage.py
+++ tests/test_wattage.py
@@ -24,4 +24,57 @@ def test_wattage_simple():
         air_density=air_density
     )
     assert pytest.approx(watts, rel=0.01) == expected_watts
-    
+
+
+def test_newton_method_linear():
+    slope = 3
+    y_int = 2
+    x_int = -y_int / slope
+    def f(x):
+        return slope * x + y_int
+    def dfdx(x):
+        return slope
+    estimated_x_int = wattage._newton_method(f, dfdx)
+    assert pytest.approx(estimated_x_int, rel=1e-4) == x_int
+
+
+def test_newton_method_quadratic():
+    def f2(x):
+        return (x-2)**2
+    def df2dx(x):
+        # (x-2)**2 = x**2 - 4x + 4
+        return 2*x - 4
+    estimated_x_int2 = wattage._newton_method(f2, df2dx, iterations=20)
+    assert pytest.approx(estimated_x_int2, rel=1e-4) == 2
+
+
+def test_velocity_simple():
+    air_density = 1.226  # kg/m**3, average @ sea-level
+    rider_mass = 75  # kg
+    bike_mass = 8  # kg
+    crr = 0.004
+    cda = 0.25
+    gradient = 0
+    wind_speed = 0  # m/s
+    expected_watts = 246.395
+    target_speed = 40 * 1000.0 / 60.0 / 60.0  # 40kph in m/s
+    watts = wattage.solve_for_watts(
+        target_velocity=target_speed,
+        acceleration=0,
+        slope=gradient,
+        total_mass=rider_mass+bike_mass,
+        relative_wind=wind_speed,
+        c_rolling=crr,
+        cda=cda,
+        air_density=air_density
+    )
+    estimated_speed = wattage.solve_for_speed(
+        watts,
+        acceleration=0,
+        slope=gradient,
+        total_mass=rider_mass+bike_mass,
+        c_rolling=crr,
+        cda=cda,
+        air_density=air_density
+    )
+    # assert estimated_speed == target_speed
--- wattage/wattage.py
+++ wattage/wattage.py
@@ -1,4 +1,5 @@
 import math
+from typing import NewType
 
 
 def solve_for_watts(target_velocity,
@@ -16,5 +17,51 @@ def solve_for_watts(target_velocity,
     air_speed = (target_velocity - relative_wind)
     aero_wattage = 0.5 * cda * air_density * air_speed**2 * target_velocity
     total = rr_wattage + pe_wattage + ke_wattage + aero_wattage
-    # print(f"RR:{rr_wattage:0.1f} PE:{pe_wattage:0.1f} KE:{ke_wattage:0.1f} AE:{aero_wattage:0.1f} TOT:{total:0.1f}")
     return total
+
+
+def _newton_method(f, fdx, initial=0, iterations=10):
+    def _newton_step(x):
+        y = f(x)
+        slope = fdx(x)
+        intercept = x - y / slope
+        error = f(intercept)
+        return intercept, error
+    
+    x, err = _newton_step(initial)
+    print(x, err)
+    for _ in range(iterations):
+        x, err = _newton_step(x)
+        print(x, err)
+    return x
+
+
+def solve_for_speed(target_watts,
+                    acceleration,
+                    slope,
+                    total_mass,
+#                     relative_wind,  # positive is tailwind
+                    c_rolling,
+                    cda,
+                    air_density):
+    def _watts(v):
+        return solve_for_watts(
+            v,
+            acceleration,
+            slope,
+            total_mass,
+            0,  # air_speed
+            c_rolling,
+            cda,
+            air_density
+        ) - target_watts
+
+    def _dWdv(v):
+        gravity = 9.81  # m/s**2
+        return (c_rolling * total_mass * gravity +
+                slope * total_mass * gravity +
+                total_mass * acceleration +
+                0.5 * cda * air_density * 3 * v**2)
+
+    # Use newton method to solve for speed
+    return _newton_method(_watts, _dWdv, initial=3, iterations=20)
