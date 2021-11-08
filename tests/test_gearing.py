0d0036dd0d6fd303b158b8557ba592fe7675cc32
bstaiger <bstaiger@data.cs.purdue.edu>
2021-11-08 10:10:47 -0600

    Add gearing helper functions

--- tests/test_gearing.py
+++ tests/test_gearing.py
@@ -1,1 +1,25 @@
 from wattage import gearing
+
+
+def test_find_cassette():
+    chainring = 53
+    ratio = chainring / 11.0
+    setups = gearing.find_cassette(chainring, ratio, rel_err=1e-4)
+    assert len(setups) == 1 
+    assert setups[0]["cassette"] == 11
+
+    setups = gearing.find_cassette(chainring, ratio, rel_err=0.1)
+    assert len(setups) == 1 
+    assert setups[0]["cassette"] == 11
+
+
+def test_find_setups():
+    chainring = 53
+    ratio = chainring / 11.0
+    setups = gearing.find_setups(ratio, rel_err=1e-4)
+    found_real_setup = False
+    print(setups)
+    for s in setups:
+        if s["cassette"] == 11 and s["chainring"] == 53:
+            found_real_setup = True
+    assert found_real_setup
--- wattage/gearing.py
+++ wattage/gearing.py
@@ -1,24 +1,45 @@
-
-from wattage import conversion
-from wattage import wattage
-from wattage import wheel
+from wattage.wattage import solve_for_speed
+from wattage.wheel import Wheel
 
 
 _chainrings = list(range(30,63,1))
 _cassettes = list(range(10,51,1))
 
 
-def find_gear_ratio(speed, ftp, incline):
-    pass
+# Maybe this should be client code. That way gearing
+# doesn't need to know anything about the modeling...
+def find_gear_ratio(watts: float, cadence: float, incline: float, wheel: Wheel, total_mass, crr=0.004, cda=0.3, air_density=1.226):
+    expected_speed = solve_for_speed(
+        watts,
+        0,  # acceleration
+        incline,
+        total_mass,
+        crr,
+        cda,
+        air_density
+    )
+    return wheel.gear_ratio(speed=expected_speed, cadence=cadence)
 
 
-def find_cassette(chainring, gear_ratio):
-    pass
+def find_cassette(chainring, gear_ratio, rel_err=0.05):
+    setups = []
+    for c in _cassettes:
+        if abs(gear_ratio - chainring / c) < rel_err:
+            setups.append({
+                "chainring": chainring,
+                "cassette": c,
+                "gear_ratio": chainring / c,
+                "rel_err": abs(gear_ratio - chainring / c),
+            })
+    return setups
 
 
-def find_setups(ratio):
+def find_setups(gear_ratio, rel_err=0.05):
     """
     return a list of chainring / cassettes that will contain the 
     desired ratio.
     """
-    pass
+    setups = []
+    for r in _chainrings:
+        setups.extend(find_cassette(r, gear_ratio, rel_err))
+    return setups
